52c6b178edfd8be174548cbb4b3b80091f5d9d2a
Filippok82 <irina.filippova.mail@yandex.ru>
2023-04-19 10:07:48 +0300

    Analysis of lecture 2

---
+++ Lesson/lesson02/test/test_01.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+def checkout(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
+    if text in result.stdout and result.returncode == 0:
+        return True
+    else:
+        return False
+
+
+def test_step1():
+    assert checkout('cd/home/zerg/tst; 7z a ../out/arx2', 'Everything is OK'), "test FAIL"
+
+
+def test_step2():
+    assert checkout('cd/home/zerg/out; 7z e arx2.7z /home/zerg/folder1', 'Everything is OK'), "test2 FAIL"
+
+
+def test_step3():
+    assert checkout('cd/home/zerg/out; 7z t arx2.7z', 'Everything is OK'), "test3 FAIL"
